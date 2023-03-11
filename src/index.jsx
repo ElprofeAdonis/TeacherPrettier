09f0e3dd7612c9b4bd191a1519b4db32b58f4a12
Adonis Aleman <adonis.aleman@unadeca.net>
2023-03-10 20:12:31 -0600

    Cmabios en los archivos y por el momento solo envio una ruta de que todo funciona bien

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Bienvenidos a la página de inicio.</p>
+    </div>
+  );
+};
+
+export default Home;
